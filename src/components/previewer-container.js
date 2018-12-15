a1d51e72799cc455ed4af1269c220cd9060b2f4b
Abdu Assabri <abdusabri@outlook.com>
2018-12-15 23:06:29 +0100

    Add syntax highlighting, update default text

--- src/components/previewer-container.js
+++ src/components/previewer-container.js
@@ -3,12 +3,19 @@ import "./previewer-container.css";
 import Editor from "./editor";
 import Preview from "./preview";
 import { parseTextToMarkdown } from "../utils/markdown-parser";
+import { getDefaultText } from "../utils/default-text";
 
 class PreviewerContainer extends Component {
   state = {
     text: "",
     markedText: ""
   };
+
+  componentDidMount() {
+    const defaultText = getDefaultText();
+    this.setState({ text: defaultText });
+    this.setState({ markedText: parseTextToMarkdown(defaultText) });
+  }
 
   onChange = event => {
     this.setState({ text: event.target.value });
--- src/utils/markdown-parser.js
+++ src/utils/markdown-parser.js
@@ -1,9 +1,16 @@
 import marked from "marked";
 import sanitizeHtml from "sanitize-html";
+import hljs from "highlight.js/lib/highlight";
+import javascript from "highlight.js/lib/languages/javascript";
+import "highlight.js/styles/github.css";
+hljs.registerLanguage("javascript", javascript);
 
 export const parseTextToMarkdown = text => {
   return marked(text, {
     sanitize: true,
-    sanitizer: sanitizeHtml
+    sanitizer: sanitizeHtml,
+    highlight: function(code) {
+      return hljs.highlightAuto(code).value;
+    }
   });
 };
