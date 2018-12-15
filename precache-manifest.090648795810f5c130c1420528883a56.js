4e49bd0266d8112fa591cad346e4160436c8020a
Abdu Assabri <abdusabri@outlook.com>
2018-12-15 23:16:22 +0100

    Updates

---
+++ precache-manifest.090648795810f5c130c1420528883a56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea79587df1b89aef09e0",
+    "url": "/fcc-markdown-previewer/static/js/runtime~main.ea79587d.js"
+  },
+  {
+    "revision": "b1e93418523e56208bbe",
+    "url": "/fcc-markdown-previewer/static/js/main.b1e93418.chunk.js"
+  },
+  {
+    "revision": "99a2120e123715b407dc",
+    "url": "/fcc-markdown-previewer/static/js/1.99a2120e.chunk.js"
+  },
+  {
+    "revision": "b1e93418523e56208bbe",
+    "url": "/fcc-markdown-previewer/static/css/main.62392a97.chunk.css"
+  },
+  {
+    "revision": "99a2120e123715b407dc",
+    "url": "/fcc-markdown-previewer/static/css/1.25cc3daa.chunk.css"
+  },
+  {
+    "revision": "871dfcca70219b37d970b40ba9b46225",
+    "url": "/fcc-markdown-previewer/index.html"
+  }
+];
